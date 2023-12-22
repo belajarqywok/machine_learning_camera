3027ec334e9c053797574752ef1e233d889c2ba7
belajarqywok <belajarqywok@gmail.com>
2023-12-22 23:40:44 +0700

    Fixing Camera Bug and RPC Responses

--- src/App.vue
+++ src/App.vue
@@ -6,131 +6,129 @@
 </template>
   
 <script>
-  import Swal from 'sweetalert2'
-  import { generateBlobImageName } from './utils'
-  
+	import Swal from 'sweetalert2'
+	import { generateBlobImageName } from './utils'
+	
 	export default {
-		data() {
+		data: () => {
 			return {
 				video: null,
-        swal: Swal
+				swal: Swal
 			}
 		},
-	
-		mounted() {
+		
+		mounted: () => {
 			this.initCamera()
 			this.setFullscreen()
 		},
-	
+		
 		methods: {
 			swalErrno: async (swal) => {
 				swal.close()
-			  swal.fire({
+				swal.fire({
 					title: 'Nginggg...!!!',
 					text: 'Upps.. Gagal Mendeteksi, coba lagi !..',
 					icon: 'error',
-					confirmButtonText: 'Kembali',
-				});
+					confirmButtonText: 'Kembali'
+				})
 			},
 
-			async initCamera() {
+			initCamera: async () => {
 				try {
 					const constraints = { video: { facingMode: 'environment' } }
 					const stream = await navigator.mediaDevices.getUserMedia(constraints)
 					this.$refs.videoElement.srcObject = stream
-
+					
 				} catch (_) { await this.swalErrno(this.swal) }
 			},
-	
-			async capturePhoto() {
+			
+			capturePhoto: async () => {
 				this.swal.fire({
 					title: 'Sedang Menganalisa Penyakit..',
 					allowOutsideClick: false,
 					showConfirmButton: false,
-					willOpen: () => {
-						this.swal.showLoading()
-					},
+					willOpen: () => { this.swal.showLoading() }
 				})
-		
+				
 				const video = this.$refs.videoElement
 				const canvas = document.createElement('canvas')
-
+				
 				canvas.width = video.videoWidth
 				canvas.height = video.videoHeight
-
+				
 				const context = canvas.getContext('2d')
 				context.drawImage(video, 0, 0, canvas.width, canvas.height)
-		
+				
 				const dataURI = canvas.toDataURL('image/jpeg')
 				const blob = this.dataURItoBlob(dataURI)
-		
+				
 				try {
 					const formData = new FormData()
 					formData.append('file', blob, await generateBlobImageName())
 		
-          const headers = new Headers();
-          headers.append('Accept', '*/*');
-          headers.append('Accept-Encoding', 'gzip, deflate, br');
-          headers.append('Connection', 'keep-alive');
-
+          			const headers = new Headers()
+          			headers.append('Accept', '*/*')
+          			headers.append('Accept-Encoding', 'gzip, deflate, br')
+          			headers.append('Connection', 'keep-alive')
+					
 					const endpoint = 'https://docpet-backend-service-rnjlx2stlq-et.a.run.app/api/disease_detection/';
 
-          const response = await fetch(endpoint, {
-            method: 'POST',
-            body: formData,
-            headers: headers,
-          });
+          			const response = await fetch(endpoint, {
+            			method: 'POST',
+            			body: formData,
+            			headers: headers
+          			})
+					
+					if (!response.ok) await this.swalErrno(this.swal)
 
-          if (!response.ok) await this.swalErrno(this.swal)
-		
 					const response_body = await response.json()
-          if (response_body.data == null) await this.swalErrno(this.swal)
-          
-          else {
-            this.swal.close()
-            this.swal.fire({
-              title: `${response_body.data.name}!`,
-              html: `
-                <br/>
-                <b>Gejala</b>
-                <p style="text-align: justify;">${response_body.data.symptom}</p><br/>
-                <b>Perawatan</b>
-                <p style="text-align: justify;">${response_body.data.treatment}</p><br/>
-                <b>Informasi</b>
-                <p style="text-align: justify;">${response_body.data.information}</p>
-              `,
-              icon: 'success',
-              confirmButtonText: 'Kembali',
-            })
-          }
-
+					
+					if (response_body.data == null) await this.swalErrno(this.swal)
+					else {
+						this.swal.close()
+						this.swal.fire({
+							title: `${response_body.data.name}!`,
+							html: `
+								<br/>
+								<b>Gejala</b>
+								<p style="text-align: justify;">${response_body.data.symptom}</p><br/>
+								<b>Perawatan</b>
+								<p style="text-align: justify;">${response_body.data.treatment}</p><br/>
+								<b>Informasi</b>
+								<p style="text-align: justify;">${response_body.data.information}</p>
+              				`,
+							icon: 'success',
+							confirmButtonText: 'Kembali'
+						})
+					}
+				
 				} catch (_) { await this.swalErrno(this.swal) }
 			},
-	
-			dataURItoBlob(dataURI) {
+			
+			dataURItoBlob: (dataURI) => {
 				const byteString = atob(dataURI.split(',')[1])
 				const mimeString = dataURI.split(',')[0]
 					.split(':')[1].split(';')[0]
-
+					
 				const ab = new ArrayBuffer(byteString.length)
 				const ia = new Uint8Array(ab)
-
+				
 				for (let i = 0; i < byteString.length; i++) {
 					ia[i] = byteString.charCodeAt(i)
 				}
-
+				
 				return new Blob([ab], { type: mimeString })
 			},
 	
-			setFullscreen() {
+			setFullscreen: () => {
 				const appElement = document.querySelector('.app')
-
+				
 				appElement.style.display = 'flex'
 				appElement.style.justifyContent = 'center'
 				appElement.style.alignItems = 'center'
 				appElement.style.height = '100vh'
 				appElement.style.backgroundColor = '#000'
-		
+				
 				const videoElement = this.$refs.videoElement
 				videoElement.style.width = '100vw'
 				videoElement.style.height = '100vh'
